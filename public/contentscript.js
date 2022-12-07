d35519dc852188f87bda0e828411784a0ce29fd6
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-12-07 12:01:25 +0000

    Addhover effect to element

--- public/contentscript.js
+++ public/contentscript.js
@@ -8,7 +8,7 @@ console.log("Value = " + euroVal)
 
 // Create the element ot be injected, giving it a class and adding text
 var injectedElement = document.createElement('div');
-injectedElement.className = "injectedElement";
+injectedElement.className = 'injectedElement';
 const injectedText = document.createTextNode(`Budget-to-Beat: ${euroVal}`);
 injectedElement.appendChild(injectedText);
 
@@ -19,7 +19,7 @@ function css(element, style) {
     }        
 }
 
-// Apply styles to injected element
+// Apply inline styles to injected element
 css(injectedElement, {
     'background-color': '#11117d',
     'color': 'white',
@@ -30,6 +30,28 @@ css(injectedElement, {
     'border-radius': '5px',
     'position': 'relative'
 });
+
+
+// Initializes style element for pseudo styles in the head of the document and append to head
+const pseudoStyles = document.createElement('style');
+pseudoStyles.innerHTML = `
+    .injectedElement:hover {
+        background-color: white!important;
+        border: #11117d 1px solid!important;
+        color: #11117d!important;
+        cursor: pointer; 
+    }
+
+    .injectedElement:hover::after {
+        content: "Click me for more info!";
+        font-size: 12px;
+        color: red;
+        text-align: center;
+        display: block;
+    }
+`
+const head = document.querySelector('head')
+head.appendChild(pseudoStyles);
 
 // Initialize parent element as a variable and attach new element to it
 const parentElement = document.querySelector(
